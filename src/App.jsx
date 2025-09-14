65789374e0d9796f3611c7a8ac9bca1e25dbc398
Gino Gregoret <gregoretgino@gmail.com>
2025-09-14 15:46:54 -0300

    dragonball

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,63 @@
-import {useState, useEffect} from 'react'
-import {Contenedor} from "./components/Contenedor"
-import {consultar} from "./Api/api.js"
-import {Tarjeta} from './components/Tarjeta.jsx'
-function App(){
-  
+import { useState, useEffect } from 'react'
+import { Contenedor } from "./components/Contenedor"
+import { consultar } from "./api/api.js"
+import { Tarjeta } from './components/Tarjeta.jsx'
+import Buscador from './buscador.jsx'
+
+function App() {
+
   const [items, setItems] = useState([])
-  const [loading,setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [personaje, BuscarPersonaje] = useState([])
+  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    async function cargar(){
+    async function cargar() {
       setLoading(true)
       const i = await consultar()
       setItems(i)
-      setLoading(false)
-
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000)
     }
     cargar()
-  },[])
-  if(loading){
-    return <p>cargando...</p>
+  }, [])
+  if (loading) {
+    return <p>Cargando....</p>
+
   }
-
-
-
-  return(
-    <div>
-      <Contenedor>
+  
+  return (
+    
+    <div className="bg-gradient-to-b from-amber-300 to-amber-600">
+      <input
+      type='text'
+      placeholder='buscar personaje'
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+        style={{
+          padding: '10px',
+          margin: '20px auto',
+          display: 'block',
+          width: '300px',
+          borderRadius: '8px',
+          border: '1px solid ',
+          fontSize: '16px'
+        }}
+      />
+      
+      <Contenedor >
         {
-          items.map((item)=>{
-            return <Tarjeta item = {item} key={item.id}></Tarjeta>
+
+          items.map((item) => {
+            return <Tarjeta item={item} key={item?.id} />
+
           })
         }
       </Contenedor>
     </div>
   )
 }
+
 export default App
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,11 +1,9 @@
 export function Tarjeta({item}){
-  
-
   return(
-  <div className="flex flex-col items-center w-[200px] h-[320px] border-2 rounded border-neutral-300">
+  <div className="flex flex-col items-center w-[200px] h-[320px] border-2 rounded border-neutral-300 bg-gradient-to-b bg-white">
     <h3>{item?.name}</h3>
     <div className="w-[200px]">
-      <img className="w-[200px] h-[290px] object-contain" src={item?.image}/>
+      <img className="w-[200px] h-[290px] object-contain justify-center" src={item?.image}/>
     </div>
     <p>{item?.ki}</p>
   </div>
